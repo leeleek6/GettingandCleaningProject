dbdead2126d5b533beebeb7cb409c38618239613
leeleek6 <knupp.19@osu.edu>
2021-06-22 09:13:20 -0400

    Adds activity names and makes data frame of std and mean features

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,26 @@
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
                 destfile = "accelerometers.zip") #downloads data folder
-gzfile("accelerometers.zip")
 unzip("accelerometers.zip")
-
-testx <- read.delim("UCI HAR Dataset/test/X_test.txt", header = FALSE)
-testy <- read.delim("UCI HAR Dataset/test/Y_test.txt", header = FALSE)
-testsubj <- read.delim("UCI HAR Dataset/test/subject_test.txt",header=FALSE)
-testset <- cbind(testx,testy,testsubj)
-colnames(testset) <- c("set","label","subject")
-
-trainx <- read.delim("UCI HAR Dataset/train/X_train.txt", header = FALSE)
-trainy <- read.delim("UCI HAR Dataset/train/Y_train.txt", header = FALSE)
-trainsubj <- read.delim("UCI HAR Dataset/train/subject_train.txt",header=FALSE)
-trainset <- cbind(trainx,trainy,trainsubj)
-colnames(trainset) <- c("set","label","subject")
 
 features <- unlist(read.delim("UCI HAR Dataset/features.txt",header = FALSE))
 features <- lapply(strsplit(features," "), function(l) l[[2]])
 activity <- unlist(read.delim("UCI HAR Dataset/activity_labels.txt",header=FALSE))
 activity <- lapply(strsplit(activity," "), function(l) l[[2]])
+
+testx <- read.delim("UCI HAR Dataset/test/X_test.txt", header = FALSE)
+testy <- read.delim("UCI HAR Dataset/test/Y_test.txt", header = FALSE)
+testy <- data.frame(unlist(activity)[as.factor(unlist(testy))])
+testsubj <- read.delim("UCI HAR Dataset/test/subject_test.txt",header=FALSE)
+testset <- cbind(testx,testy,testsubj)
+colnames(testset) <- c("dataset","activity","subject")
+
+trainx <- read.delim("UCI HAR Dataset/train/X_train.txt", header = FALSE)
+trainy <- read.delim("UCI HAR Dataset/train/Y_train.txt", header = FALSE)
+trainy <- data.frame(unlist(activity)[as.factor(unlist(trainy))])
+trainsubj <- read.delim("UCI HAR Dataset/train/subject_train.txt",header=FALSE)
+trainset <- cbind(trainx,trainy,trainsubj)
+colnames(trainset) <- c("dataset","activity","subject")
+
 
 dat <- rbind(testset,trainset)
 
@@ -32,8 +34,9 @@ seprows <- function(column) {
   out
 }
 #creates new table with each set in a row. each entry in a set is assigned to proper feature
-newrows <- seprows(dat$set)
+newrows <- seprows(dat$dataset)
 df <- data.frame(matrix(unlist(newrows), nrow=length(newrows), byrow=TRUE))
 colnames(df) <- features
+df <- select(df,contains("mean") | contains("std"))
 
-
+#unlist(activity)[as.factor(t)] fix
