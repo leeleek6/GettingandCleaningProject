9a3eecb71ee658517eb15579ffd0693ac84df59f
leeleek6 <knupp.19@osu.edu>
2021-06-22 11:57:41 -0400

    Added comments and changed when variables were renamed

--- run_analysis.R
+++ run_analysis.R
@@ -1,15 +1,18 @@
+##downloading data and loading appropriate packages
 download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
-                destfile = "accelerometers.zip") #downloads data folder
+                destfile = "accelerometers.zip") 
 unzip("accelerometers.zip")
 library(dplyr)
 library(tidyr)
 
+##reading activities and features
 features <- unlist(read.delim("UCI HAR Dataset/features.txt",header = FALSE))
 features <- lapply(strsplit(features," "), function(l) l[[2]])
 activity <- unlist(read.delim("UCI HAR Dataset/activity_labels.txt",header=FALSE))
 activity <- lapply(strsplit(activity," "), function(l) l[[2]])
 activity <- sub("_"," ",tolower(activity))
 
+##reading/binding test data, uses descriptive activity names
 testx <- read.delim("UCI HAR Dataset/test/X_test.txt", header = FALSE)
 testy <- read.delim("UCI HAR Dataset/test/Y_test.txt", header = FALSE)
 testy <- data.frame(unlist(activity)[as.factor(unlist(testy))])
@@ -17,6 +20,7 @@ testsubj <- read.delim("UCI HAR Dataset/test/subject_test.txt",header=FALSE)
 testset <- cbind(testsubj,testy,testx)
 colnames(testset) <- c("subject","activity","dataset")
 
+##reading/binding train data, uses descriptive activity names
 trainx <- read.delim("UCI HAR Dataset/train/X_train.txt", header = FALSE)
 trainy <- read.delim("UCI HAR Dataset/train/Y_train.txt", header = FALSE)
 trainy <- data.frame(unlist(activity)[as.factor(unlist(trainy))])
@@ -24,10 +28,10 @@ trainsubj <- read.delim("UCI HAR Dataset/train/subject_train.txt",header=FALSE)
 trainset <- cbind(trainsubj,trainy,trainx)
 colnames(trainset) <- c("subject","activity","dataset")
 
-
+##merge train and test data
 dat <- arrange(rbind(testset,trainset),subject)
 
-#gets set column in dat formatted into separate numbers
+#seprows formats entries in a column of data as separate numeric
 seprows <- function(column) {
   out <- list()
   for(i in 1:length(column)) {
@@ -36,17 +40,26 @@ seprows <- function(column) {
   }
   out
 }
-#creates new table with each set in a row. each entry in a set is assigned to proper feature
+
+##separates data in dataset column and creates dataframe with each value associated with corresponding variable
 newrows <- seprows(dat$dataset)
 df <- data.frame(matrix(unlist(newrows), nrow=length(newrows), byrow=TRUE))
 colnames(df) <- features
+
+##extracts variables for std and mean only
 df <- select(df,contains("mean") | contains("std"))
 vartable <- cbind(select(dat,subject,activity),df)
+
+
+##descriptively renames variables
+colnames(vartable) <- sub("-mean","Mean",names(vartable))
+colnames(vartable) <- sub("-std","Std",names(vartable))
+colnames(vartable) <- sub("-X","X",names(vartable))
+colnames(vartable) <- sub("-Y","Y",names(vartable))
+colnames(vartable) <- sub("-Z","Z",names(vartable))
+colnames(vartable) <- sub("BodyBody","Body",names(vartable))
+colnames(vartable) <- sub("\\()","",names(vartable))
+
+##creates dataframe with average of each variable for each activity and subject
 means <- vartable %>% group_by(subject,activity) %>% summarize_all(list(mean))
-colnames(means) <- sub("-mean","Mean",names(means))
-colnames(means) <- sub("-std","Std",names(means))
-colnames(means) <- sub("-X","X",names(means))
-colnames(means) <- sub("-Y","Y",names(means))
-colnames(means) <- sub("-Z","Z",names(means))
-colnames(means) <- sub("BodyBody","Body",names(means))
-colnames(means) <- sub("\\()","",names(means))
+
